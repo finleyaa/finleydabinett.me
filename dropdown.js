f6c5b7e67d9eff677658b3e922ef3ddf02f5f24f
F1R3Kael <finleytorterra@gmail.com>
2017-09-22 23:47:19 +0100

    Uploading website files

---
+++ dropdown.js
@@ -0,0 +1,11 @@
+function searchDropDown(){
+	document.getElementById("search-box").classList.toggle("show")
+}
+
+function addressDropDown(){
+	document.getElementById("address-box").classList.toggle("show")
+}
+
+function emailDropDown(){
+	document.getElementById("email-box").classList.toggle("show")
+}
---
+++ finishButton.js
@@ -0,0 +1,5 @@
+function finishForm(){
+	console.log(document.getElementById("fullname-text").value)
+	console.log(document.getElementById("subject-text").value)
+	console.log(document.getElementById("message-text").value)
+}
